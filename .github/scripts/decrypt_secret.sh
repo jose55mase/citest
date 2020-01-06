47b2beb29b5ec487e183504e5174d8ce1b23729c
josemase55@gmail.com <jose55mase>
2020-01-06 20:20:40 +0100

    mas intentos

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$joxeci" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
+--output $HOME/secrets/my_secret.txt my_secret.txt.gpg
