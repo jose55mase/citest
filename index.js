95bd997ce13c71b8e3b7ef728429ac7ce5a22f0b
josemase55@gmail.com <jose55mase>
2020-01-06 21:00:03 +0100

    mas intentos pero para master

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const http = require('http');
 const server = http.createServer(function (req, res) {
     res.writeHead(200, {'content-type': 'text/plain'});
-    res.end('Hola Mundo');
+    res.end('Hola Mundo esto es de joxe master');
 });
 server.listen(8000);
