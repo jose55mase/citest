4d36607450a9ef11f474198c6be84da8213db7eb
josemase55@gmail.com <jose55mase>
2020-01-06 19:26:47 +0100

    se suben de nuevo el encriptado

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,5 +3,5 @@
 # Decrypt the file
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$joxeci" \
 --output $HOME/secrets/my_secret.json my_secret.json.gpg
